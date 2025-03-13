f4dfb229e398bb1ddcceea2a5faea3b065d5a117
Afonso Sousa <203120110+asousa-link@users.noreply.github.com>
2025-03-13 17:32:16 +0000

    Added project overall schema.

---
+++ investment-calculator-app/calculator.ts
@@ -0,0 +1,13 @@
+/* data:
+ * initial amount
+ * annual contribution
+ * expected return
+ * duration */
+
+// function calculateInvestment(data) {} // => result[]
+
+// function printResults(results) {}
+
+// const results = calculateInvestment(...);
+
+// printResults(results);
