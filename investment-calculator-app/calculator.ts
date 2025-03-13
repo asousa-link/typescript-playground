de59b5676ff86843bf0b5ce0cdcb98d7aa9cf42d
Afonso Sousa <203120110+asousa-link@users.noreply.github.com>
2025-03-13 17:53:49 +0000

    Calculator: Defining the calculateInvestment function.

--- investment-calculator-app/calculator.ts
+++ investment-calculator-app/calculator.ts
@@ -12,15 +12,49 @@ type InvestmentData = {
 };
 
 type InvestmentResult = {
-    year: number;
+    year: string;
     totalAmount: number;
     totalContributions: number;
     totalInterestEarned: number;
 };
 
 function calculateInvestment(data: InvestmentData): InvestmentResult[] | string {
+    const { initialAmount, annualContribution, expectedReturn, duration } = data;
 
-} // => result[]
+    if (initialAmount < 0) {
+        return "Initial investment amount must be at least zero."
+    }
+
+    if (expectedReturn < 0) {
+        return "Expected return must be at least zero."
+    }
+
+    if (duration < 0) {
+        return "Duration must be a valid number of years."
+    }
+
+    let total = initialAmount;
+    let totalContributions = 0;
+    let totalInterestEarned = 0;
+
+    const annualResults: InvestmentResult[] = [];
+
+    for (let i = 0; i < duration; i++) {
+        total *= (1 + expectedReturn);
+        totalInterestEarned = total - totalContributions - initialAmount;
+        totalContributions += annualContribution;
+        total += annualContribution;
+
+        annualResults.push({
+            year: `Year ${i + 1}`,
+            totalAmount: total,
+            totalInterestEarned,
+            totalContributions
+        });
+    }
+
+    return annualResults;
+}
 
 // function printResults(results) {}
 
