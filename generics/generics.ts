e3e50c786f4f0f886dcb6bbea7d156c0f9c15c5f
Afonso Sousa <203120110+asousa-link@users.noreply.github.com>
2025-03-14 17:16:22 +0000

    Playing with multiple generic parameters.

--- generics/generics.ts
+++ generics/generics.ts
@@ -10,8 +10,17 @@ store.isInstructor = true;
 
 let nameStore: DataStore<string> = {};
 
-function merge<T>(a: T, b: T): T[] {
+function merge<T, U>(a: T, b: U) {
     return [a, b];
 }
 
 const ids = merge(1, 2);
+const varied = merge('a', 1);
+
+
+function mergeObj<T extends object, U extends object>(a: T, b: U) {
+    return { ...a, ...b }
+}
+
+const merged = mergeObj({ userName: "Afonso" }, { age: 31 });
+console.log(merged);
