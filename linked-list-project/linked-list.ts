3462319877df7f4c2b8dc0cbbbc8cedc93d806aa
Afonso Sousa <203120110+asousa-link@users.noreply.github.com>
2025-03-14 17:33:04 +0000

    Linked list: Created add() method.

--- linked-list-project/linked-list.ts
+++ linked-list-project/linked-list.ts
@@ -1,10 +1,25 @@
-class ListNode {}
+class ListNode<T> {
+    next?: ListNode<T>;
+    constructor (public value: T) {}
+}
 
 class LinkedList<T> {
-    private root?: ListNode;
+    private root?: ListNode<T>;
+    private tail?: ListNode<T>;
     private length = 0;
 
-    add(value: T) {}
+    add(value: T) {
+        const node = new ListNode(value);
+        if (!this.root || !this.tail) {
+            this.root = node;
+            this.tail = node;
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.length++;
+    }
+
 }
 
 const list = new LinkedList();
