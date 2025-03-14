6bbe3d471a1777e8aa484e2a505a8e09ea5a46a0
Afonso Sousa <203120110+asousa-link@users.noreply.github.com>
2025-03-14 17:46:46 +0000

    Linked list: Printing.

--- linked-list-project/linked-list.ts
+++ linked-list-project/linked-list.ts
@@ -6,7 +6,7 @@ class ListNode<T> {
 class LinkedList<T> {
     private root?: ListNode<T>;
     private tail?: ListNode<T>;
-    private length = 0;
+    private _length = 0;
 
     add(value: T) {
         const node = new ListNode(value);
@@ -17,9 +17,29 @@ class LinkedList<T> {
             this.tail.next = node;
             this.tail = node;
         }
-        this.length++;
+        this._length++;
+    }
+
+    get length(): number { return this.length; }
+
+    print() {
+        let current = this.root;
+        while (current) {
+            console.log(current.value);
+            current = current.next;
+        }
     }
 
 }
 
 const list = new LinkedList();
+const numberList = new LinkedList<number>();
+
+numberList.add(10);
+numberList.add(5);
+numberList.add(-3);
+
+console.log(numberList.length);
+numberList.print();
+
+const nameList = new LinkedList<string>();
