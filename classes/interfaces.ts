f0f022003b40d614dfa4e9a32c5ca284ef163f4d
Afonso Sousa <203120110+asousa-link@users.noreply.github.com>
2025-03-14 14:55:55 +0000

    Playing with interface declaration merging.

--- classes/interfaces.ts
+++ classes/interfaces.ts
@@ -6,11 +6,17 @@ interface Authenticatable {
     logout(): void;
 }
 
+// extend the interface (e.g. if it came from a library)
+interface Authenticatable {
+    role: string;
+}
+
 let user: Authenticatable;
 
 user = {
     email: "test@example.com",
     password: "abc1",
+    role: "user",
     login() {
         // ...
     },
