1450fb517c42f553239a918f3f510697038ba06e
Afonso Sousa <203120110+asousa-link@users.noreply.github.com>
2025-03-14 17:47:16 +0000

    Linked list: Finished product.

--- linked-list-project/linked-list.ts
+++ linked-list-project/linked-list.ts
@@ -20,7 +20,54 @@ class LinkedList<T> {
         this._length++;
     }
 
-    get length(): number { return this.length; }
+    get length(): number { return this._length; }
+
+    insertAt(value: T, pos: number) {
+        if (pos > -1 && pos < this.length && this.root) {
+            let current = this.root;
+            let index = 0;
+            let previous = current;
+            let node = new ListNode(value);
+
+            if (pos === 0) {
+                node.next = this.root;
+                this.root = node;
+            } else {
+                while (index++ < pos && current.next) {
+                    previous = current;
+                    current = current.next;
+                }
+                node.next = current;
+                previous.next = node;
+            }
+            this._length++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    removeAt(pos: number) {
+        if (pos > -1 && pos < this.length && this.root) {
+            let current = this.root;
+            let previous: ListNode<T> = current;
+            let index = 0;
+
+            if (pos === 0) {
+                this.root = current.next;
+            } else {
+                while (index++ < pos && current.next) {
+                    previous = current;
+                    current = current.next;
+                }
+                previous.next = current.next;
+            }
+            this._length--;
+            return current;
+        } else {
+            return null;
+        }
+    }
 
     print() {
         let current = this.root;
