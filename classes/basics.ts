f9d6911f26dafdca58e7519e77b02b634ef069c6
Afonso Sousa <203120110+asousa-link@users.noreply.github.com>
2025-03-14 12:19:45 +0000

    Playing with public and private keywords.

--- classes/basics.ts
+++ classes/basics.ts
@@ -10,10 +10,19 @@
 
 // Alternative
 class User {
-    constructor (public name: string, public age: number) { }
+    public hobbies: string[] = [];
+
+    constructor (private name: string, public age: number) {}
+
+    greet() {
+        console.log("My name is " + this.name + " and I'm " + this.age + " years old.")
+    }
 };
 
 const afonso = new User("Afonso", 31);
 const fred = new User("Fred", 35);
 
 console.log(afonso, fred);
+
+fred.age = 33;
+fred.greet();
