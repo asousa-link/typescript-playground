6004c953fc70d8a9f2ca17ca22372b2966c27714
Afonso Sousa <203120110+asousa-link@users.noreply.github.com>
2025-03-14 16:45:00 +0000

    Playing with "as const".

--- advanced/advanced.ts
+++ advanced/advanced.ts
@@ -9,3 +9,7 @@ store.id = 5;
 store.isOpen = false;
 //store.name = "Ok"; // Doesn't work
 // store.1 = false; // Doesn't work
+
+let roles = ['admin', 'guest', 'editor'] as const;
+// roles.push("max"); // Doesn't work
+const firstRole = roles[0];
