bd91bdb5b3ea739d60c7e9e3fd69a7923e540f3a
Afonso Sousa <203120110+asousa-link@users.noreply.github.com>
2025-03-14 16:07:57 +0000

    Playing with instanceof type guards.

--- advanced/guards.ts
+++ advanced/guards.ts
@@ -4,7 +4,7 @@ const fileSource: FileSource = {
   path: 'some/path/to/file.csv',
 };
 
-type DBSource = { type: 'db'; connectionUrl: string };
+type DBSource = { type: 'db', connectionUrl: string };
 const dbSource: DBSource = {
   type: 'db',
   connectionUrl: 'some-connection-url',
@@ -13,11 +13,42 @@ const dbSource: DBSource = {
 type Source = FileSource | DBSource;
 
 function loadData(source: Source) {
-  // Open + read file OR reach out to database server
-  if (source.type === "file") {
-    // source.path;
+  // if ('path' in source) {
+  if (source.type === 'file') {
+    // source.path
+    // source.path; => use that to open the file
+    return;
+  }
+  // source.connectionUrl; => to reach out to database
+}
+
+// With classes we can use instanceof
+class User {
+  constructor (public name: string) {}
+
+  join() {
+    // ...
+  }
+}
+
+class Admin {
+  constructor (permissions: string[]) {}
+
+  scan() {
+    // ...
+  }
+}
+
+const user = new User('Max');
+const admin = new Admin(['ban', 'restore']);
+
+type Entity = User | Admin;
+
+function init(entity: Entity) {
+  if (entity instanceof User) {
+    entity.join();
     return;
   }
 
-  // source.connectionUrl;
+  entity.scan();
 }
