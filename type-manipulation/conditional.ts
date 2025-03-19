0a5ddbb3aecd7fc53727040d3ae7be0d57f08cf1
Afonso Sousa <203120110+asousa-link@users.noreply.github.com>
2025-03-19 11:25:29 +0000

    Playing with conditional types some more.

--- type-manipulation/conditional.ts
+++ type-manipulation/conditional.ts
@@ -6,3 +6,17 @@ type GetElementType<T> = T extends any[] ? T[number] : T; // returns T if it's n
 let text = "abc";
 type Example = GetElementType<typeof text>;
 type Example2 = GetElementType<StringArray>;
+
+type FullNamePerson = {firstName: string; lastName: string};
+type FullnameOrNothing<T> = T extends FullNamePerson ? string : never;
+
+function getFullname<T extends object>(person: T) : FullnameOrNothing<T> {
+    if ('firstName' in person && 'lastName' in person && person.firstName && person.lastName) {
+        return `${person.firstName} ${person.lastName}` as FullnameOrNothing<T>;
+    };
+    throw new Error('No first/last name found.')
+};
+
+const name1 = getFullname({});
+const name2 = getFullname({firstName: "Afonso"});
+const name3 = getFullname({firstName: "Afonso", lastName: "Sousa"});
