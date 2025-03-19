df7b82004bd271dba4a90419ec829a3891116792
Afonso Sousa <203120110+asousa-link@users.noreply.github.com>
2025-03-19 15:31:08 +0000

    Added a property logger, accessor logger, method logger and parameter logger.

--- decorators-experimental/src/app.ts
+++ decorators-experimental/src/app.ts
@@ -32,3 +32,62 @@ class Person {
 const pers = new Person();
 
 console.log(pers);
+
+function LogProperty(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator");
+  console.log(target, propertyName);
+}
+
+function LogAccessor(
+  target: any,
+  name: string,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Accessor decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function LogMethod(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Method decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function LogParameter(target: any, name: string | Symbol, position: number) {
+  console.log("Parameter decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
+class Product {
+  @LogProperty
+  title: string;
+  private _price: number;
+
+  @LogAccessor
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @LogMethod
+  getPriceWithTax(@LogParameter tax: number) {
+    return this._price * (1 + tax);
+  }
+}
