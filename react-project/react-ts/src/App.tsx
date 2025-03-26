769f63786ec9f528f7623f41aa36cf37f19dfd49
Afonso Sousa <203120110+asousa-link@users.noreply.github.com>
2025-03-26 12:14:04 +0000

    Added the CourseGoals component.

--- react-project/react-ts/src/App.tsx
+++ react-project/react-ts/src/App.tsx
@@ -1,13 +1,29 @@
-import Header from './components/Header.tsx';
-import goalsImg from './assets/goals.jpg'
+import Header from "./components/Header.tsx";
+import goalsImg from "./assets/goals.jpg";
+import CourseGoals from "./components/CourseGoals.tsx";
 
 function App() {
-
   return (
     <main>
-      <Header image={{src: goalsImg, alt: "A list of goals"}}/>
+      <Header image={{ src: goalsImg, alt: "A list of goals" }}>
+        <h1>Your Course Goals</h1>
+      </Header>
+      <CourseGoals
+        goals={[
+          {
+            id: 1,
+            title: "Learn TS",
+            description: "Learn TS from the ground up",
+          },
+          {
+            id: 2,
+            title: "Learn React",
+            description: "Learn React from the ground up",
+          },
+        ]}
+      />
     </main>
   );
 }
 
-export default App
+export default App;
