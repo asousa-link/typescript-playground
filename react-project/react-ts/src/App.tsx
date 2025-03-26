8e82911d9382de2a4fa75327c96af90caeb2aabc
Afonso Sousa <203120110+asousa-link@users.noreply.github.com>
2025-03-26 12:37:32 +0000

    Added the New Goal component.

--- react-project/react-ts/src/App.tsx
+++ react-project/react-ts/src/App.tsx
@@ -2,6 +2,7 @@ import Header from "./components/Header.tsx";
 import goalsImg from "./assets/goals.jpg";
 import CourseGoals from "./components/CourseGoals.tsx";
 import { useState } from "react";
+import NewGoal from "./components/NewGoal.tsx";
 
 function App() {
 
@@ -21,12 +22,17 @@ function App() {
   function handleDeleteGoal(id: number) {
     setGoals((prevGoals) => prevGoals.filter(g => g.id !== id));
   }
+
+  function handleAddGoal(text: string, summary: string) {
+    setGoals(prev => prev.concat({id: Math.random(), title: text, description: summary}))
+  }
   
   return (
     <main>
       <Header image={{ src: goalsImg, alt: "A list of goals" }}>
         <h1>Your Course Goals</h1>
       </Header>
+      <NewGoal onAdd={handleAddGoal} />
       <CourseGoals
         goals={goals} onDelete={handleDeleteGoal}
       />
