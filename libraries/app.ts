d0ad796c85e0b4e0b0707d100ffdd636412ab577
Afonso Sousa <203120110+asousa-link@users.noreply.github.com>
2025-03-26 10:35:04 +0000

    Managing typescript schema validation with Zod.

--- libraries/app.ts
+++ libraries/app.ts
@@ -1,5 +1,25 @@
 import _ from "lodash";
+import { z } from "zod";
+import fs from "node:fs";
 
-const numbers = [1, 2, 3, 4, 5];
+//const numbers = [1, 2, 3, 4, 5];
 
-const chunkedArr = _.chunk(numbers, 2);
+//const chunkedArr = _.chunk(numbers, 2);
+
+const dataSchema = z.object({
+  title: z.string(),
+  id: z.number(),
+  values: z.array(z.union([z.string(), z.number()])),
+});
+
+type Data = z.infer<typeof dataSchema>;
+
+function output(data: Data) {
+  console.log(data);
+}
+
+const content = JSON.parse(fs.readFileSync("data.json").toString());
+
+const parsedData = dataSchema.parse(content);
+
+output(parsedData);
