41a8bee7b006a467093028282736a6a75c4f0cbd
Afonso Sousa <203120110+asousa-link@users.noreply.github.com>
2025-03-26 11:08:41 +0000

    Setting up the index page and hooking the project to the OSM API.

--- place-maps-project/src/app.ts
+++ place-maps-project/src/app.ts
@@ -1,1 +1,28 @@
-// Code goes here!
+import axios from "axios";
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  axios
+    .get(
+      `https://nominatim.openstreetmap.org/search?q=${encodeURI(
+        enteredAddress
+      )}&format=json`
+    )
+    .then((response) => {
+      const data = response.data[0];
+      const lat = data.lat;
+      const long = data.lon;
+
+      const coordinates = { lat: lat, long: long };
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+form.addEventListener("submit", searchAddressHandler);
