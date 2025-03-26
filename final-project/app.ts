8969445d5fd816cbce206398bd633c3aabfe8125
Afonso Sousa <203120110+asousa-link@users.noreply.github.com>
2025-03-26 15:04:54 +0000

    Added CRUD routes.

--- final-project/app.ts
+++ final-project/app.ts
@@ -1,8 +1,13 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import todoRoutes from "./routes/todo.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(todoRoutes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: "An error occurred!", error: err });
+});
+
 app.listen(3000);
--- final-project/routes/todo.ts
+++ final-project/routes/todo.ts
@@ -1,5 +1,5 @@
 import express, { type Request, type Response } from "express";
-import { addTodo } from "../data.js";
+import { addTodo, getTodo, getTodos, removeTodo, updateTodo } from "../data.js";
 
 const router = express.Router();
 
@@ -12,5 +12,25 @@ function handlePostTodos(req: Request, res: Response) {
 }
 
 router.post("/todos", handlePostTodos);
+
+router.get("/todos", (req, res) => {
+  const todos = getTodos();
+  res.json({ todos });
+});
+
+router.get("/todos/:id", (req, res) => {
+  const todo = getTodo(+req.params.id);
+  res.json({ todo });
+});
+
+router.patch("/todos/:id", (req, res) => {
+  const updatedTodo = updateTodo(+req.params.id, req.body.text);
+  res.json({ message: "Todo updated!", todo: updatedTodo });
+});
+
+router.delete("/todos/:id", (req, res) => {
+  removeTodo(+req.params.id);
+  res.json({ message: "Todo deleted!" });
+});
 
 export default router;
