336230d22a0cb77b1b9e3cbf3df41844f0b3944a
Afonso Sousa <203120110+asousa-link@users.noreply.github.com>
2025-03-26 12:21:58 +0000

    Added the Goals state and working delete.

--- react-project/react-ts/src/App.tsx
+++ react-project/react-ts/src/App.tsx
@@ -1,26 +1,34 @@
 import Header from "./components/Header.tsx";
 import goalsImg from "./assets/goals.jpg";
 import CourseGoals from "./components/CourseGoals.tsx";
+import { useState } from "react";
 
 function App() {
+
+  const [goals, setGoals] = useState([
+    {
+      id: 1,
+      title: "Learn TS",
+      description: "Learn TS from the ground up",
+    },
+    {
+      id: 2,
+      title: "Learn React",
+      description: "Learn React from the ground up",
+    },
+  ]);
+
+  function handleDeleteGoal(id: number) {
+    setGoals((prevGoals) => prevGoals.filter(g => g.id !== id));
+  }
+  
   return (
     <main>
       <Header image={{ src: goalsImg, alt: "A list of goals" }}>
         <h1>Your Course Goals</h1>
       </Header>
       <CourseGoals
-        goals={[
-          {
-            id: 1,
-            title: "Learn TS",
-            description: "Learn TS from the ground up",
-          },
-          {
-            id: 2,
-            title: "Learn React",
-            description: "Learn React from the ground up",
-          },
-        ]}
+        goals={goals} onDelete={handleDeleteGoal}
       />
     </main>
   );
